12fe3dba3a7fe293ca6e96f8b5e764884471b45f
Alec Delaney <tekktrik@gmail.com>
2022-05-02 12:53:41 -0400

    Require Blinka>=7.2.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     # Author details
     author="Adafruit Industries",
     author_email="circuitpython@adafruit.com",
-    install_requires=["Adafruit-Blinka>=7.0.0"],
+    install_requires=["Adafruit-Blinka>=7.2.3"],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
